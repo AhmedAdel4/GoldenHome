4b2af83a371a2241d7a9ab2a63cc6c3d74b799b0
Ahmed Adel <ahmedcrspo55@gmail.com>
2020-03-09 15:35:41 +0200

    updating admin panel

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.app')
 @section('content')
 
-
-
+<h1 style="text-align: center;padding: 5%; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">Golden Home</h1>
+<p style="text-align: center;padding: 5%; font-size: 80px;">مرحبا بك فى صفحة الأداره</p>
 @endsection
--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -1,9 +1,8 @@
 @if ($header != null)
 <header class="masthead" style="background-image: url({{ '/images/header/'.$header->image}}) !important">    
     <div class="container">
-      <div class="intro-text">
+      <div class="intro-text" style="padding-top: 137px; padding-bottom: 100px">
         <h1 class="text-primary"> {!! htmlspecialchars_decode($header->text) !!}</h1>
-        
         <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#portfolio">تصفح المنتجات</a>
       </div>
     </div>
